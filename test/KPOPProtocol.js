07a593d447f7a3290f0912d5428f9d37ae539a93
kimsyna <109972663+kimsyna@users.noreply.github.com>
2025-08-20 14:23:40 +0900

    refactor: rename KPPToken to KPOP Protocol

--- test/KPOPProtocol.js
+++ test/KPOPProtocol.js
@@ -1,21 +1,21 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("KPPToken", function () {
+describe("KPOPProtocol", function () {
   it("assigns initial supply to deployer", async function () {
     const [owner] = await ethers.getSigners();
-    const KPPToken = await ethers.getContractFactory("KPPToken");
+    const KPOPProtocol = await ethers.getContractFactory("KPOPProtocol");
     const initialSupply = ethers.parseUnits("1000000", 18);
-    const token = await KPPToken.deploy(initialSupply);
+    const token = await KPOPProtocol.deploy(initialSupply);
     await token.waitForDeployment();
     expect(await token.balanceOf(owner.address)).to.equal(initialSupply);
   });
 
   it("transfers tokens between accounts", async function () {
     const [owner, addr1, addr2] = await ethers.getSigners();
-    const KPPToken = await ethers.getContractFactory("KPPToken");
+    const KPOPProtocol = await ethers.getContractFactory("KPOPProtocol");
     const initialSupply = ethers.parseUnits("1000000", 18);
-    const token = await KPPToken.deploy(initialSupply);
+    const token = await KPOPProtocol.deploy(initialSupply);
     await token.waitForDeployment();
 
     await token.transfer(addr1.address, ethers.parseUnits("100", 18));
