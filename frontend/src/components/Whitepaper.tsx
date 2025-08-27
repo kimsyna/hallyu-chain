4b42a81a459e21fe72c0d108119827c7c5514d5c
kimsyna <109972663+kimsyna@users.noreply.github.com>
2025-08-27 15:03:40 +0900

    Load whitepaper dynamically

--- frontend/src/components/Whitepaper.tsx
+++ frontend/src/components/Whitepaper.tsx
@@ -1,14 +1,34 @@
-import { Trans } from 'react-i18next'
+import { useEffect, useState } from 'react'
+import { Trans, useTranslation } from 'react-i18next'
+import createDOMPurify from 'dompurify'
+
+const DOMPurify = createDOMPurify(window)
 
 function Whitepaper() {
+  const { i18n, t } = useTranslation()
+  const [html, setHtml] = useState<string | null>()
+
+  useEffect(() => {
+    fetch(`whitepaper/${i18n.language}/index.html`)
+      .then((response) => (response.ok ? response.text() : Promise.reject()))
+      .then((data) => {
+        setHtml(DOMPurify.sanitize(data))
+      })
+      .catch(() => {
+        setHtml(null)
+      })
+  }, [i18n.language])
+
   return (
     <section id="whitepaper">
       <h2>
         <Trans i18nKey="nav_whitepaper" />
       </h2>
-      <p>
-        <Trans i18nKey="notice_whitepaper_unavailable" />
-      </p>
+      {html ? (
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      ) : html === null ? (
+        <p>{t('notice_whitepaper_unavailable')}</p>
+      ) : null}
     </section>
   )
 }
