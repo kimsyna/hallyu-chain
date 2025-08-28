d4d3fb72650c87614653ef7d70f370d1b6f309c5
kimsyna <109972663+kimsyna@users.noreply.github.com>
2025-08-28 15:31:00 +0900

    feat: add dynamic resources section

--- frontend/src/components/Resources.tsx
+++ frontend/src/components/Resources.tsx
@@ -1,41 +1,47 @@
-import { Trans } from 'react-i18next'
+import { useEffect, useState } from 'react';
+import { Trans } from 'react-i18next';
+
+interface Resource {
+  nameKey: string;
+  icon: string;
+  url: string;
+}
 
 function Resources() {
+  const [items, setItems] = useState<Resource[]>([]);
+
+  useEffect(() => {
+    fetch('/resources.json')
+      .then((res) => res.json())
+      .then(setItems)
+      .catch((err) => console.error('Failed to load resources:', err));
+  }, []);
+
   return (
     <section id="resources">
       <h2>
         <Trans i18nKey="resources_title" />
       </h2>
       <ul className="icon-list">
-        <li>
-          <i className="material-symbols-outlined" aria-hidden="true">insert_drive_file</i>
-          <a href="#whitepaper">
-            <Trans i18nKey="res_whitepaper" />
-          </a>
-        </li>
-        <li>
-          <i className="material-symbols-outlined" aria-hidden="true">code</i>
-          <a
-            href="https://github.com/hallyu-chain"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Trans i18nKey="res_github" />
-          </a>
-        </li>
-        <li>
-          <i className="material-symbols-outlined" aria-hidden="true">chat</i>
-          <a
-            href="https://discord.gg/example"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Trans i18nKey="res_discord" />
-          </a>
-        </li>
+        {items.map((item) => (
+          <li key={item.nameKey}>
+            <i className="material-symbols-outlined" aria-hidden="true">
+              {item.icon}
+            </i>
+            <a
+              href={item.url}
+              target={item.url.startsWith('http') ? '_blank' : undefined}
+              rel={
+                item.url.startsWith('http') ? 'noopener noreferrer' : undefined
+              }
+            >
+              <Trans i18nKey={item.nameKey} />
+            </a>
+          </li>
+        ))}
       </ul>
     </section>
-  )
+  );
 }
 
-export default Resources
+export default Resources;
